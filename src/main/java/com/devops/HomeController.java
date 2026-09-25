f5afa9ef369378b28a345cde6fc8c8451587a2bf
Likithav <likithaveeragoni007@gmail.com>
2026-09-25 22:13:59 +0530

    update application message for CI/CD test

--- src/main/java/com/devops/HomeController.java
+++ src/main/java/com/devops/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello from Jenkins CI/CD - version 2";
+        return "Hello from Jenkins CI/CD - version 2 , Maven Application is Running Successfully.";
     }
 }
